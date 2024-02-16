5ba733fd5596c73ee853aa77b8eb1c9755da192c
Emmanuelle Rebosura <em374081@ucf.edu>
2024-02-16 03:10:38 -0500

    Checking for duplicates in Autochecker
    Note: i didn't test if this works yet, i'm just pushing what i've done so far

--- M-MO-VR Simulation/Assets/Editor/AutoChecker.cs
+++ M-MO-VR Simulation/Assets/Editor/AutoChecker.cs
@@ -27,35 +27,59 @@ using AccessibilityTags;
 
 public class AutoChecker : Editor
 {    
+    AccessibilityTags.AccessibilityTags script = selectedObject.GetComponent<AccessibilityTags.AccessibilityTags>();
+
     if (script.AltText != null)
     {
         // If altText is empty
         if (script.AltText == "")
         {
             // run AutoAccessibility.cs to fill it out probably or alert developer
+            AutoAccessibility.AddField();
         }
         // If altText is filled out
         else
         {
-            // Check if altText is a duplicate (same name as another object or ends with a number)
-            
-            // code here to compare altText of one object to altText of another object
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
 
-            // Check if altText is only one word
-            if (script.AltText.Contains(" "))
-            {
-                Debug.Log("Alt Text is more than one word for " + obj.name);
-            }
-            else
-            {
-                // alert developer asking if they are sure about keeping it as one word
+                // Check if altText is a duplicate (same name as another object or ends with a number)
+                CheckForDuplicateAltText(object, script);
+                // code here to compare altText of one object to altText of another object (above)
+
+                // Check if altText is only one word
+                if (script.AltText.Contains(" "))
+                {
+                    Debug.Log("Alt Text is more than one word for " + object.name);
+                }
+                else
+                {
+                    // alert developer asking if they are sure about keeping it as one word
+                }
             }
         }
     }
     else
     {
-        Debug.Log("Alt Text is null for " + obj.name);
+        Debug.Log("Alt Text is null for " + object.name);
         // alert developer
     }
 
+    private void CheckForDuplicateAltText(GameObject obj, AccessibilityTags.AccessibilityTags script)
+    {
+        GameObject[] objectsInScene = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject otherObj in objectsInScene)
+        {
+            if (otherObj != obj)
+            {
+                AccessibilityTags.AccessibilityTags otherScript = otherObj.GetComponent<AccessibilityTags.AccessibilityTags>();
+                if (otherScript != null && script.AltText == otherScript.AltText)
+                {
+                    Debug.LogWarning("Duplicate altText found for objects: " + obj.name + " and " + otherObj.name);
+                }
+            }
+        }
+    }
 }
